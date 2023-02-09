d34900500b64743d96b060d20c52bd5678461732
ymk <ymg4798@naver.com>
2023-02-09 17:12:57 +0900

    2023-02-09 Valid Test

---
+++ src/main/java/com/springbootstudy/controller/dto/Valids.java
@@ -0,0 +1,12 @@
+package com.springbootstudy.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Valids {
+    private String name;
+    private Integer age;
+    private String value;
+}
